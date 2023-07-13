2db8aba532f103f556827e6aea0af49759d4d112
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2023-07-13 14:31:52 -0700

    Writing a c program using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,9 @@
  *
  * Return: 0 (success)
  */
-
 int main(void)
 
 {
-	puts("\"Programming is like building a mulilingual puzzle ");
-
-	return (0);
+puts("\"Programming is like building a mulilingual puzzle ");
+return (0);
 }
