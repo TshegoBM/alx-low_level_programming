669b126e0d08d1be5a1e3adc08edb46a6ac1d1e8
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2023-07-13 13:29:08 -0700

    Writing a c program using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-/* A program that prints exactly Programming is like building a multilingual puzzle followed by a new line */
-
 int main(void)
 
 {
-	puts("Programming is like building a mulilingual puzzle");
+	puts("\"Programming is like building a mulilingual puzzle");
+
 	return (0);
 }
