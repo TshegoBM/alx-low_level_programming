09faf590e48cc0c279883ef860b8b38f3a464cb0
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2023-07-13 13:14:52 -0700

    Writing a c program using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* A program that prints exactly Programming is like building a multilingual puzzle followed by a new line */
+
+int main(void)
+
+{
+	puts("Programming is like building a mulilingual puzzle");
+	return (0);
+}
