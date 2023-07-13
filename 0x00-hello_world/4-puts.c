abb6394ade5de55a006597b49383d9da17447290
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2023-07-13 13:49:03 -0700

    Writing a c program using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+/**
+ * This program demonstrates the use of the puts function to print a message to the console.
+ */
 int main(void)
 
 {
-	puts("\"Programming is like building a mulilingual puzzle");
+	puts("\"Programming is like building a mulilingual puzzle ");
 
 	return (0);
 }
