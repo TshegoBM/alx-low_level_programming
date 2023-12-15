15187db2ac1adb6dfba2e3040dbe5bba789dd790
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2023-12-15 13:25:28 -0800

    Print numbers 0 - 9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print numbers 1 - 10
+ * Return: 0 (success)
+ */
+int main(void)
+{
+        char i = 48;
+
+        while (i <= 57)
+        {
+                putchar(i);
+                i++;
+        }
+
+        putchar('\n');
+        return (0);
+}
