bbe0a5337fc9e5b782286659f386fddbc3f0f3cb
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2023-12-15 11:50:51 -0800

    Print the alphabet in lowecase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: Print the alphabet in lowercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char a = 97;
+	
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
