c77e786956e466a2bef10456f17956ec002eb155
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2023-12-15 13:06:19 -0800

    Print all letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print the alphabet excep q and e
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char a = 97;
+	
+	while (a <= 122)
+	{
+		if (a != 101 && a != 113)
+	{
+		putchar(a);
+	}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
