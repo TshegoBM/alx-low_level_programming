a8fd2abf59cd5d4890a9cbad170f9ff5525c4300
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2023-12-15 20:27:05 -0800

    Print all combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print all combinations of single digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		putchar(' ');
+	if (a < 57)
+	{
+		putchar(',');
+	}
+
+	a++;
+
+	}
+	putchar('\n');
+
+	return (0);
+}
+
