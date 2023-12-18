b4476049479e9864eb53bd8d09de6af28d3e3af3
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2023-12-18 00:12:43 -0800

    Print all combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,30 +8,29 @@ int main(void)
 {
 	int a = 48;
 	int b = 49;
-	
-	while(b <= 57)
+
+	while (b <= 57)
 	{
 		putchar(a);
 		putchar(b);
 
-	if(a != 56 || b != 57)
+	if (a != 56 || b != 57)
 	{
 		putchar (',');
 		putchar(' ');
 	}
 
 	b++;
-	
-	if(b > 57)
+
+	if (b > 57)
 	{
 
 		a++;
 		b = a + 1;
-	
+
 	}
 	}
 	putchar ('\n');
 
 	return (0);
 }
-	
