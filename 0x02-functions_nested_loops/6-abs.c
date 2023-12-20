86f2878869c9f1efb3fbe32f84402273a06fe25e
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2023-12-20 12:16:48 -0800

    Print the absolute value of a number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
  * _abs - computes the absolute value of an integer
+ * @a: characher to be checked
+ * Return: a
  */
 int _abs(int a)
 {
 	if (a < 0)
-	
+
 	a = -a;
-	
+
 	else
 
 	_putchar(a);
-	
+
 	return (a);
 }
