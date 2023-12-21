fa2b80586f8c0cd5e6ddc303cfa7f2b2ff3cda8d
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2023-12-21 07:11:09 -0800

    Check for uppercase character

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Prints numbers from 0 to 9,
+ * followed by a new line.
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
