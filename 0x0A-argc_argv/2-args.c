af483cb53556cc3ed4786dba22b7e48b507e6259
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2024-01-12 21:16:46 -0800

    Print arguments with argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * argv - Prints all arguments its receives.
+ * main - Prints all arguments its receives.
  * @argc: Argument count.
  * @argv: Argument value.
  *
- * Return 0 - (success)
+ * Return: 0 - (success)
  */
 
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
