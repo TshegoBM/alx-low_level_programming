d172269db0483da8c5ba424eb9a61b897daeb942
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2024-01-22 11:21:07 -0800

    Implement __FILE__ macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Prints the name of the file it was compiled from,
+ *		followed by a new line.
+ *
+ * Return: void
+ */
 
-int main (void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
