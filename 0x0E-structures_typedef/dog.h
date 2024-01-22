139c4514e2804355ed067a157ea08ff47b144848
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2024-01-22 14:22:01 -0800

    Structure representing information about a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - Structure representing information about a dog.
+ * @name: Pointer to a string representing the dog's name.
+ * @age: Float representing the age of the dog.
+ * @owner: Pointer to a string representing the owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
