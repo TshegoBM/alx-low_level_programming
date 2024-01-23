d749de9022b36d31f357f7efed4033f69796f3c0
Tshegofatso <motsuenyanetshegofatso@gmail.com>
2024-01-23 14:57:20 -0800

    New struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,17 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - Structure representing information about a dog.
  * @name: Pointer to a string representing the dog's name.
  * @age: Float representing the age of the dog.
  * @owner: Pointer to a string representing the owner's name.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
